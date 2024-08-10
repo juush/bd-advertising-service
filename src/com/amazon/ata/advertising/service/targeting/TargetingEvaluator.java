be96ff02fdf3b38353212efc8744d20becb3c7bb
Josh <jp.jacques@pm.me>
2024-08-09 19:49:22 -0500

    finished p1

--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -5,6 +5,7 @@ import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicate
 import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicateResult;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Evaluates TargetingPredicates for a given RequestContext.
@@ -29,17 +30,12 @@ public class TargetingEvaluator {
      * @return TRUE if all of the TargetingPredicates evaluate to TRUE against the RequestContext, FALSE otherwise.
      */
     public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
-        List<TargetingPredicate> targetingPredicates = targetingGroup.getTargetingPredicates();
-        boolean allTruePredicates = true;
-        for (TargetingPredicate predicate : targetingPredicates) {
-            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
-            if (!predicateResult.isTrue()) {
-                allTruePredicates = false;
-                break;
-            }
-        }
-
-        return allTruePredicates ? TargetingPredicateResult.TRUE :
-                                   TargetingPredicateResult.FALSE;
+        Optional<List<TargetingPredicate>> targetingPredicates = Optional.of(targetingGroup.getTargetingPredicates());
+        boolean allTruePredicates = targetingPredicates
+                .orElse(List.of())
+                .stream()
+                .map(predicate -> predicate.evaluate(requestContext))
+                .allMatch(TargetingPredicateResult::isTrue);
+        return allTruePredicates ? TargetingPredicateResult.TRUE : TargetingPredicateResult.FALSE;
     }
 }
